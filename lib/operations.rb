eeed6876526ccd3423bfdfaf770608d89e3c7575
Viviana Lugo <vivi28lu@gmail.com>
2020-09-14 20:08:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
-def unsafe?(speed)
-
+def unsafe?(speed=60)
+  if speed = 50 == speed
+    false
+  else
+    true
+  end
 end
 
 
 
-def not_safe?(speed)
-	
+def not_safe?(speed = 50)
+  speed = 50 == speed ? false : true
 end
 	
 
